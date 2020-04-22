66c04af07be76ded18e8e8d6ff234771a40f8dc7
Ghani Hikmawan <ghani@paxel.co>
2020-04-22 11:53:26 +0700

    Find My Age Application complete

--- app/src/main/java/dev/ghani/findmyage/MainActivity.kt
+++ app/src/main/java/dev/ghani/findmyage/MainActivity.kt
@@ -2,11 +2,32 @@ package dev.ghani.findmyage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnFind.setOnClickListener{
+           getAge()
+        }
+    }
+
+    fun getAge(){
+        try {
+            val userDOB = Integer.parseInt(etDOB.text.toString())
+            val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+            val userAge = currentYear  - userDOB
+
+            tvAge.text = "Your Age is $userAge Years"
+        } catch (ex:Exception){
+            Log.d("Error", "bukan angka " + ex.message)
+        }
+
     }
 }
